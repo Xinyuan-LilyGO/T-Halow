b83b21ce6d4fdf51a166cd6a292d55939a4157e1
ShallowGreen <duanguangxin2022@gmail.com>
2024-03-22 10:01:55 +0800

    TF card and TX_AH_Rx00p module are successfully verified.

---
+++ examples/TF_card/utilities.h
@@ -0,0 +1,40 @@
+
+#pragma once
+
+// Camera Shield pinmap
+#define CAMERA_PIN_PWDN     (-1)
+#define CAMERA_PIN_RESET    (18)
+#define CAMERA_PIN_XCLK     (8)
+#define CAMERA_PIN_SIOD     (2)
+#define CAMERA_PIN_SIOC     (1)
+
+#define CAMERA_PIN_Y9       (9)
+#define CAMERA_PIN_Y8       (10)
+#define CAMERA_PIN_Y7       (11)
+#define CAMERA_PIN_Y6       (13)
+#define CAMERA_PIN_Y5       (21)
+#define CAMERA_PIN_Y4       (48)
+#define CAMERA_PIN_Y3       (47)
+#define CAMERA_PIN_Y2       (14)
+
+#define CAMERA_PIN_VSYNC    (16)
+#define CAMERA_PIN_HREF     (17)
+#define CAMERA_PIN_PCLK     (12)
+
+#define XCLK_FREQ_HZ        20000000
+
+// TF card
+#define TF_SPI_CS   39
+#define TF_SPI_SCK  41
+#define TF_SPI_MOSI 40
+#define TF_SPI_MISO 42
+
+// AH-Rx00P
+#define SerialMon     Serial
+#define SerialAT      Serial1
+#define SERIAL_AT_RXD 5
+#define SERIAL_AT_TXD 4
+//
+
+
+
